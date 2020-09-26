bbe3986dcdb039ac44b0d1ad70c25f3d5ab4b092
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-09-26 21:39:10 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
 
   has_one :identification
   has_one :deliver_address
+  has_many :cards
   has_many :items,foreign_key: :seller_id, dependent: :destroy
+  has_many :sns_credentials
+
   accepts_nested_attributes_for :identification
   accepts_nested_attributes_for :deliver_address
 
@@ -16,9 +20,20 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*\z/
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
 
-  has_many :items,foreign_key: :seller_id, dependent: :destroy
-  has_many :cards
-
-  
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
 
 end
