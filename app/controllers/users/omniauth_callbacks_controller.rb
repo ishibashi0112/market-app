98cd1d5f952a7f4d24d33a3a97e2590480f62c48
masaaki1983 <saikyou_sw@yahoo.co.jp>
2020-09-26 21:40:45 +0900

    omniauth-controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -20,6 +20,29 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def failure
   #   super
   # end
+  def facebook
+    authorization
+  end
+
+  def google_oauth2
+    authorization
+  end
+
+  def failure
+    redirect_to root_path
+  end
+
+  private
+
+  def authorization
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      render template: 'devise/registrations/new'
+    end
+  end
 
   # protected
 
