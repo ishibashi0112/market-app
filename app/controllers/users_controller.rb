320cd331826acc1790287747a60dd7651450f0b7
kinugawayushi <070707yushi@gmail.com>
2020-10-01 17:43:52 +0900

    マイページ　コントローラーの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,19 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
+  before_action :set_items, only: [:show,:index_seller_page,:index_buyer_page]
+  before_action :set_seller_items, only: [:show,:index_seller_page]
+  before_action :set_buyer_items, only: [:show,:index_buyer_page]
+  before_action :set_pagenate, only: [:index_seller_page,:index_buyer_page]
+  
+
   def show
-    @items = current_user.items
-    @selling_id = Item.where(seller_id:current_user.id)
-    @buying_id = Item.where(buyer_id:current_user.id)
   end
 
 
-  def index_seller_page
-    @items = current_user.items
-    @selling_id = Item.where(seller_id:current_user.id)
-    @items = Item.includes(:images).order("created_at DESC").page(params[:page]).per(12)
+  def index_seller_page   
   end
 
   def index_buyer_page
-    @items = current_user.items
-    @buying_id = Item.where(buyer_id:current_user.id)
-    @items = Item.includes(:images).order("created_at DESC").page(params[:page]).per(12)
   end
 
   private
@@ -24,6 +21,23 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+  
+  def set_items
+    @items = current_user.items
+  end
+
+  def set_seller_items
+    @selling_items = Item.where(seller_id:current_user.id)
+  end
+
+  def set_buyer_items
+    @buying_items = Item.where(buyer_id:current_user.id)
+  end
+
+  def set_pagenate
+    @items = Item.includes(:images).order("created_at DESC").page(params[:page]).per(12)
+  end
+
   def user_params
     params.fetch(:user, {}).permit(:nickname).merge(seller_id: current_user.id, trading_status: 0)
   end
