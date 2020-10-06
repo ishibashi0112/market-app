a5065304d66203d19342cf5db8f4666c9f1b5e3d
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2020-10-06 20:09:43 +0900

    rspec_fix
    ・購入機能テストコード修正

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
   factory :card do
     card_id              {"car_d0e44730f83b0a19ba6caee04160"}
     customer_id          {"cus_c8521968ef0f3f016bb1e33ad233"}
-    user_id              {"1"}
+    user                 { FactoryBot.create(:user)}
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -9,19 +9,19 @@ describe Card do
     it "card_idがない場合は登録できないこと" do
       card = build(:card, card_id: nil)
       card.valid?
-      expect(card.errors[:card_id]).to include("translation missing: ja.activerecord.errors.models.card.attributes.card_id.blank")
+      expect(card.errors[:card_id]).to include("を入力してください")
     end
 
     it "customer_idがない場合は登録できないこと" do
       card = build(:card, customer_id: nil)
       card.valid?
-      expect(card.errors[:customer_id]).to include("translation missing: ja.activerecord.errors.models.card.attributes.customer_id.blank")
+      expect(card.errors[:customer_id]).to include("を入力してください")
     end
 
     it "user_idがない場合は登録できないこと" do
       card = build(:card, user_id: nil)
       card.valid?
-      expect(card.errors[:user_id]).to include("translation missing: ja.activerecord.errors.models.card.attributes.user_id.blank")
+      expect(card.errors[:user_id]).to include("を入力してください")
     end
   end
 end
